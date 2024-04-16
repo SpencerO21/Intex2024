ea31b20ea637338235970644e70f56548f18543a
Spencer021 <63733047+MrCheif@users.noreply.github.com>
2024-04-16 09:18:03 -0600

    Test to see if google was our problem

--- Intex2024/Program.cs
+++ Intex2024/Program.cs
@@ -13,11 +13,11 @@ var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
 
-services.AddAuthentication().AddGoogle(googleOptions =>
-{
-    googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
-    googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
-});
+// services.AddAuthentication().AddGoogle(googleOptions =>
+// {
+//     googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
+//     googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+// });
  // Add services to the container.
 builder.Services.AddSingleton<InferenceSession>(provider =>
 {
